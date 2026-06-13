1039b28543ba4968b8cfbce6262f2a5043f4434f
adilhusain01 <husainadil202@gmail.com>
2026-06-14 03:21:50 +0530

    feat: add redirects for www.printngift.store to printngift.store

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.printngift.store" }],
+        destination: "https://printngift.store/:path*",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
